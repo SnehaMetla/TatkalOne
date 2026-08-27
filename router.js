3e9a0b64770c9155760be6ab5fcb77848fabb165
MMETLA SNEHA SAI SRI <snehametla30@gmail.com>
2026-08-27 23:41:09 +0530

    Delete router.js

--- router.js
+++
@@ -1,7 +0,0 @@
-import { renderRoute } from './flows/booking-flow.js';
-
-/** Renders the initial hash and every later hash change. */
-export function initRouter() {
-  window.addEventListener('hashchange', () => renderRoute((window.location.hash || '#/home').replace(/^#\//, '')));
-  renderRoute((window.location.hash || '#/home').replace(/^#\//, ''));
-}
